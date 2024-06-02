47078ee172419123c94d46c2fcbf16ac5d19d8f1
v16s <16210686+v16s@users.noreply.github.com>
2024-06-02 23:20:04 +0530

    emits logs

--- src.ts/metashrew-view.ts
+++ src.ts/metashrew-view.ts
@@ -5,7 +5,6 @@ import { getLogger } from "./logger";
 import EventEmitter from "events";
 import rlp from "rlp";
 import { ethers } from "ethers";
-import chunk from "lodash/chunk";
 
 const DB_LOCATION = process.env.DB_LOCATION;
 
@@ -13,21 +12,21 @@ const logger = getLogger();
 
 export const readArrayBufferAsUtf8 = (
   memory: WebAssembly.Memory,
-  ptr: number,
+  ptr: number
 ) => {
   return Buffer.from(
-    Array.from(new Uint8Array(readArrayBuffer(memory, ptr))),
+    Array.from(new Uint8Array(readArrayBuffer(memory, ptr)))
   ).toString("utf8");
 };
 
 export const readArrayBufferAsHex = (
   memory: WebAssembly.Memory,
-  ptr: number,
+  ptr: number
 ) => {
   return (
     "0x" +
     Buffer.from(
-      Array.from(new Uint8Array(readArrayBuffer(memory, ptr))),
+      Array.from(new Uint8Array(readArrayBuffer(memory, ptr)))
     ).toString("hex")
   );
 };
@@ -44,9 +43,11 @@ const addHexPrefix = (s) => (s.substr(0, 2) === "0x" ? s : "0x" + s);
 
 async function get(db: any, key: Buffer): Promise<Buffer> {
   try {
-    return await new Promise((resolve, reject) => db.get(key, (err, result) => err ? reject(err) : resolve(result)));
+    return await new Promise((resolve, reject) =>
+      db.get(key, (err, result) => (err ? reject(err) : resolve(result)))
+    );
   } catch (e) {
-    if (String(e).match('NotFound')) return Buffer.from([]);
+    if (String(e).match("NotFound")) return Buffer.from([]);
     else throw e;
   }
 }
@@ -56,12 +57,16 @@ function swizzle(bytes: Buffer): Buffer {
 }
 
 function leftPad(hex: string, n: number): string {
-  return '0'.repeat(n - hex.length) + hex;
+  return "0".repeat(n - hex.length) + hex;
 }
 
 function makeIndexKey(key: Buffer, length: number): Buffer {
   const data = Array.from(key);
-  return Buffer.from(data.concat(Array.from(swizzle(Buffer.from(leftPad(length.toString(16), 8), 'hex')))));
+  return Buffer.from(
+    data.concat(
+      Array.from(swizzle(Buffer.from(leftPad(length.toString(16), 8), "hex")))
+    )
+  );
 }
 
 function makeLengthKey(key: Buffer): Buffer {
@@ -69,17 +74,22 @@ function makeLengthKey(key: Buffer): Buffer {
 }
 
 function bytesToNumber<T>(bytes: Array<T> | Buffer): number {
-  return Number('0x' + Buffer.from(bytes as any).toString('hex'));
+  return Number("0x" + Buffer.from(bytes as any).toString("hex"));
 }
 
-async function getValueForBlock(db: any, key: Buffer, blockTag: string): Promise<Buffer> {
-  const blockNumber = blockTag === 'latest' ? 0xffffffff : Number(blockTag);
-  let i = (await getLengthAtKey(db, key) - 1);
+async function getValueForBlock(
+  db: any,
+  key: Buffer,
+  blockTag: string
+): Promise<Buffer> {
+  const blockNumber = blockTag === "latest" ? 0xffffffff : Number(blockTag);
+  let i = (await getLengthAtKey(db, key)) - 1;
+  //logger.info("keylength: " + i);
   while (i >= 0) {
     const result = Array.from(await get(db, makeIndexKey(key, i)));
     const resultHeight = bytesToNumber(result.slice(-4));
     if (resultHeight <= blockNumber) return Buffer.from(result.slice(0, -4));
-    i--
+    i--;
   }
   return Buffer.from([]);
 }
@@ -87,7 +97,7 @@ async function getValueForBlock(db: any, key: Buffer, blockTag: string): Promise
 export async function getLengthAtKey(db: any, key: Buffer): Promise<number> {
   const length = await get(db, makeLengthKey(key));
   if (length.length === 0) return 0;
-  return Number('0x' + Buffer.from(length as any).toString('hex'));
+  return Number("0x" + Buffer.from(length as any).toString("hex"));
 }
 
 export class IndexSandbox extends EventEmitter {
@@ -101,10 +111,12 @@ export class IndexSandbox extends EventEmitter {
     this.db = rocksdb(DB_LOCATION);
   }
   async openDatabase() {
-    return await new Promise((resolve, reject) => this.db.open({ readOnly: true }, (err) => {
-      if (err) return reject(err);
-      resolve(this);
-    }));
+    return await new Promise((resolve, reject) =>
+      this.db.open({ readOnly: true }, (err) => {
+        if (err) return reject(err);
+        resolve(this);
+      })
+    );
   }
   get memory() {
     return (this as any).instance.instance.exports.memory;
@@ -115,7 +127,7 @@ export class IndexSandbox extends EventEmitter {
     const length = data.readUInt32LE(ptr - 4);
     this.emit(
       "log",
-      Buffer.from(ary.slice(ptr, ptr + length)).toString("utf8"),
+      Buffer.from(ary.slice(ptr, ptr + length)).toString("utf8")
     );
   }
   __load_input(ptr: number): void {
@@ -131,7 +143,11 @@ export class IndexSandbox extends EventEmitter {
   __flush(v: number): void {}
   async __get(blockTag: string, k: number, v: number): Promise<void> {
     const key = readArrayBufferAsHex(this.memory, k);
-    const value = await getValueForBlock(this.db, Buffer.from(key.substr(2), 'hex'), blockTag);
+    const value = await getValueForBlock(
+      this.db,
+      Buffer.from(key.substr(2), "hex"),
+      blockTag
+    );
     const view = new Uint8Array(this.memory.buffer);
     const valueData = value;
     for (let i = 0; i < valueData.length; i++) {
@@ -140,7 +156,11 @@ export class IndexSandbox extends EventEmitter {
   }
   async __get_len(blockTag: string, k: number): Promise<number> {
     const key = readArrayBufferAsHex(this.memory, k);
-    const value: Buffer = await getValueForBlock(this.db, Buffer.from(key.substr(2), 'hex'), blockTag);
+    const value: Buffer = await getValueForBlock(
+      this.db,
+      Buffer.from(key.substr(2), "hex"),
+      blockTag
+    );
     return value.length;
   }
   abort() {
@@ -173,49 +193,65 @@ export function dumpJSONRPCPayload(payload) {
 }
 
 export function logMiddleware(req, res, next) {
-  const ip = req.get("X-Real-IP") || '127.0.0.1';
+  const ip = req.get("X-Real-IP") || "127.0.0.1";
   logger.info(ip + "|" + dumpJSONRPCPayload(req.body));
   next();
 }
 
 export async function run(program: ArrayBuffer) {
-  const _programHash = ethers.solidityPackedKeccak256(['bytes'], [addHexPrefix(Buffer.from(Array.from(new Uint8Array(program))).toString('hex'))]);
-  logger.info('program hash: ' + _programHash);
+  const _programHash = ethers.solidityPackedKeccak256(
+    ["bytes"],
+    [
+      addHexPrefix(
+        Buffer.from(Array.from(new Uint8Array(program))).toString("hex")
+      ),
+    ]
+  );
+  logger.info("program hash: " + _programHash);
   const app = express();
   app.use(bodyParser.json());
   app.use(logMiddleware);
-  app.post('/', (req, res) => {
+  app.post("/", (req, res) => {
     (async () => {
       if (!req.body) return res.json({ success: "NO" });
       const { id, method, params } = req.body;
       try {
-        if (method === 'metashrew_view') {
-          const [ programHash, fn, input, blockTag ] = params;
-          if (addHexPrefix(programHash) !== _programHash) throw Error('program hash invalid for process handler');
+        if (method === "metashrew_view") {
+          const [programHash, fn, input, blockTag] = params;
+          if (addHexPrefix(programHash) !== _programHash)
+            throw Error("program hash invalid for process handler");
+          logger.info("input: " + input);
           const sandbox = new IndexSandbox(program);
-	  sandbox.setInput(input);
-	  await sandbox.openDatabase();
-	  const ptr = await sandbox.run(fn, blockTag);
-	  res.json({
+          sandbox.setInput(input);
+          await sandbox.openDatabase();
+          sandbox.on("log", (log) => logger.debug(log));
+          const ptr = await sandbox.run(fn, blockTag);
+          res.json({
             id,
-	    result: readArrayBufferAsHex(sandbox.memory, ptr),
-	    jsonrpc: '2.0'
-	  });
-	} else {
+            result: readArrayBufferAsHex(sandbox.memory, ptr),
+            jsonrpc: "2.0",
+          });
+        } else {
           throw Error(`method "${method}" not handled by metashrew-view`);
-	}
+        }
       } catch (e) {
-	logger.error(e);
+        logger.error(e);
         res.json({
-          jsonrpc: '2.0',
-	  id,
-	  error: e.message
-	});
+          jsonrpc: "2.0",
+          id,
+          error: e.message,
+        });
       }
     })().catch((err) => logger.error(err));
   });
-  return await new Promise((resolve, reject) => app.listen(process.env.PORT || 3000, process.env.HOST || '0.0.0.0', (err) => {
-    if (err) reject(err);
-    else resolve(app);
-  }));
+  return await new Promise((resolve, reject) =>
+    app.listen(
+      process.env.PORT || 3000,
+      process.env.HOST || "0.0.0.0",
+      (err) => {
+        if (err) reject(err);
+        else resolve(app);
+      }
+    )
+  );
 }
